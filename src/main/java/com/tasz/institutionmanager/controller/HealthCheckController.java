9f2375abc02ebb92e219187f7b8412eec08bd69a
Attila Szilard Toth <ati.toth@gmail.com>
2019-05-27 18:09:31 -0400

    Add logging, add CrossOrigin annotation

--- src/main/java/com/tasz/institutionmanager/controller/HealthCheckController.java
+++ src/main/java/com/tasz/institutionmanager/controller/HealthCheckController.java
@@ -1,14 +1,23 @@
 package com.tasz.institutionmanager.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 public class HealthCheckController {
-    @GetMapping(value = "/health-check")
+    @GetMapping(value = "/health-check", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
-    String healthCheck() {
-        return "Look ma' I'm running!";
+    Map<String, String> healthCheck() {
+        log.info("Health check called!");
+        return Collections.singletonMap("response", "Look ma' I'm running!");
     }
 }
--- src/main/java/com/tasz/institutionmanager/controller/LoginController.java
+++ src/main/java/com/tasz/institutionmanager/controller/LoginController.java
@@ -4,14 +4,18 @@ import com.tasz.institutionmanager.model.LoginData;
 import com.tasz.institutionmanager.model.User;
 import com.tasz.institutionmanager.service.LoginService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @Controller
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class LoginController {
 
     private LoginService loginService;
@@ -19,6 +23,7 @@ public class LoginController {
     @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
     public @ResponseBody
     User login(@RequestBody LoginData loginData) {
+        log.info("User is attempting to login: {}", loginData.getUsername());
         return loginService.getUserByUserName(loginData.getUsername());
     }
 }
