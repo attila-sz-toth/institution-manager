2f1bb292db02f8613f3d6897f191946571716959
Attila Szilard Toth <ati.toth@gmail.com>
2019-09-23 22:14:16 -0400

    Add institution and user related tables

--- src/main/sql/ddl/business_domain.sql
+++ src/main/sql/ddl/business_domain.sql
@@ -1,3 +1,31 @@
 INSERT INTO institutionm.roles (id, role) VALUES
 (0, 'ADMIN'),
 (1, 'EMPLOYEE');
+
+insert into institutionm.institution_types (id, type) VALUES
+(0, 'IDOSEK_OTTHONA'),
+(1, 'CSALADI_BOLCSODE'),
+(2, 'REHABILITACIO'),
+(3, 'HAZI_SEGITSEGNYUJTAS'),
+(4, 'NEVELOSZULOI_HALOZAT');
+
+INSERT INTO institutionm.care_types (id, care_type) VALUES
+(0, 'IDOSEK_OTTHONA'),
+(1, 'HAZI_SEGITSEG_NYUJTAS'),
+(2, 'CSALADI_BOLCSODE'),
+(3, 'SZENVEDELYBETEGEK_REHABILITACIOJA'),
+(4, 'CSALADI_BOLCSODE'),
+(5, 'CSALADOK_ATMENETI_OTTHONA'),
+(6, 'KULSO_FEROHELY'),
+(7, 'OTTHONT_NYUJTO_ELLATAS'),
+(8, 'KULONLEGES_ELLATAS'),
+(9, 'UTOGONDOZOI_ELLATAS');
+
+insert into institutionm.institutions (id, name, type_id) values
+(0, 'Budapesti Idosek Otthona', 0),
+(1, 'New Jersey Rehabilitation', 2),
+(2, 'Kiskunszentborzasztoi Bolcsod', 1);
+
+insert into institutionm.institutions_to_users (institution_id, user_id) VALUES
+(0, 37),
+(1, 37);
--- src/main/sql/ddl/create_tables.sql
+++ src/main/sql/ddl/create_tables.sql
@@ -1,13 +1,42 @@
-CREATE TABLE institutionm.roles (
+CREATE TABLE IF NOT EXISTS institutionm.roles (
   id   INTEGER      NOT NULL,
   role VARCHAR(255) NOT NULL,
   PRIMARY KEY (id)
 );
 
-CREATE TABLE institutionm.users (
+create table IF NOT EXISTS institutionm.institution_types (
+  id   INTEGER NOT NULL,
+  type varchar(255),
+  primary key (id)
+);
+
+create table IF NOT EXISTS institutionm.care_types (
+  id        integer not null,
+  care_type varchar(255),
+  primary key (id)
+);
+
+CREATE TABLE IF NOT EXISTS institutionm.users (
   id       INTEGER      NOT NULL,
   username VARCHAR(255) NOT NULL UNIQUE,
   password VARCHAR(255),
   role_id  INTEGER      NOT NULL REFERENCES institutionm.roles (id),
   PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS institutionm.institutions (
+  id      INTEGER NOT NULL,
+  name    varchar(255),
+  type_id integer not null references institutionm.institution_types (id),
+  primary key (id)
+);
+
+create table IF NOT EXISTS institutionm.institutions_to_types (
+  institution_id      integer not null references institutionm.institutions (id),
+  institution_type_id integer not null references institutionm.institution_types (id)
+);
+
+create table IF NOT EXISTS institutionm.institutions_to_users (
+  institution_id integer not null references institutionm.institutions (id),
+  user_id        integer not null references institutionm.users (id)
 );
